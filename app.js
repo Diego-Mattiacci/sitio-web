fe99e0ebd7b873ee01837b32eab5bccfdb87df91
Diego-Mattiacci <dmattiacci@hotmail.com>
2021-01-06 20:01:25 -0300

    Creando Mercado Liebre

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicPath= path.join(__dirname, "./public");
+app.use(express.static (publicPath));
+
+app.listen(3030, () => {
+    console.log("servidor corriendo en el puerto 3030")
+})
+
+app.get("/", (req , res) => {
+    res.sendFile (path.join(__dirname,"/views/home.html"))
+})
